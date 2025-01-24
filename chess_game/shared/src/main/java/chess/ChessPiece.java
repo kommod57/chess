5750fd5800fb76e407ea766f078071e89ae68f3b
kommod57 <kommod57@gmail.com>
2025-01-24 10:18:23 -0700

    added piece stuff

--- chess_game/shared/src/main/java/chess/ChessPiece.java
+++ chess_game/shared/src/main/java/chess/ChessPiece.java
@@ -14,7 +14,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private  final ChessGame.TeamColor pieceColor;
+    private  final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**

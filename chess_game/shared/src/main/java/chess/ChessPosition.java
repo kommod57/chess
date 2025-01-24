799f4a548b756663f0e7fca27947cb2734ce83a3
kommod57 <kommod57@gmail.com>
2025-01-24 10:27:35 -0700

    added chess exception

--- chess_game/shared/src/main/java/chess/ChessPosition.java
+++ chess_game/shared/src/main/java/chess/ChessPosition.java
@@ -14,6 +14,9 @@ public class ChessPosition {
     private final int col;
 
     public ChessPosition(int row, int col) {
+        if (row < 1 || col < 1 || row > 8 || col > 8) {
+            throw new IllegalArgumentException("Invalid row or column");
+        }
         this.row = row;
         this.col = col;
     }

b6b5bd9a33f0a5b49b94e3354385da9796d927c6
kommod57 <kommod57@gmail.com>
2025-08-04 16:44:40 -0600

    end points

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,13 +1,20 @@
 package server;
 
 import spark.*;
-
+import server.handler.*;
 
 public class Server {
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
+        Spark.delete("/db", (Route) new ClearHandler());
+        Spark.post("/user", (Route) new RegisterHandler());
+        Spark.post("/session", (Route) new LoginHandler());
+        Spark.delete("/session", (Route) new LogoutHandler());
+        Spark.get("/game", (Route) new ListGamesHandler());
+        Spark.post("/game", (Route) new CreateGameHandler());
+        Spark.put("/game", (Route) new JoinGameHandler());
 
         Spark.staticFiles.location("web");
 

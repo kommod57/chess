a28b2d7d1f5bf6dc14d037704c8b6c6fcc7411f8
kommod57 <kommod57@gmail.com>
2025-09-04 11:35:03 -0600

    chess piece

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,9 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -9,8 +12,33 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
-
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "pieceColor=" + pieceColor +
+                ", type=" + type +
+                '}';
     }
 
     /**
@@ -29,14 +57,37 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return this.pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return this.type;
+    }
+
+    //For Promotion
+    public static void addPromotionMoves(ArrayList<ChessMove> moves, ChessPosition from, ChessPosition to) {
+        for (PieceType type : Arrays.asList(
+                PieceType.QUEEN,
+                PieceType.BISHOP,
+                PieceType.KNIGHT,
+                PieceType.ROOK)) {
+            moves.add(new ChessMove(from, to, type));
+        }
+    }
+
+    // Castling
+    private boolean canCastle(ChessBoard board, int row, int... cols) {
+        for (int i=1; i < cols.length - 1; i++) {
+            ChessPiece piece = board.getPiece(new ChessPosition(row, cols[i]));
+            if (piece != null) return false; //Pathway must be empty
+        }
+        ChessPiece rook = board.getPiece(new ChessPosition(row, cols[cols.length - 1]));
+        return rook != null &&
+                rook.getPieceType() == PieceType.ROOK &&
+                rook.getTeamColor() == this.getTeamColor();
     }
 
     /**
@@ -46,6 +97,7 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
+
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         throw new RuntimeException("Not implemented");
     }

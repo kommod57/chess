a10ec6d791c28ba9a7f7bad4c944a29517712e90
kommod57 <kommod57@gmail.com>
2025-09-19 12:50:08 -0600

    pawn

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -29,7 +29,6 @@ public class ChessPiece {
         return pieceColor == that.pieceColor && type == that.type;
     }
 
-    
     @Override
     public int hashCode() {
         return Objects.hash(pieceColor, type);
